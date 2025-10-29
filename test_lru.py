6ace7a348352ae61d08d3580b27f215ea90a6b91
pujabhartiTechie <bpuja0783@gmail.com>
2025-10-29 12:02:29 +0530

    Update test_lru.py

--- test_lru.py
+++ test_lru.py
@@ -1,18 +1,18 @@
 from lru_cache import LRUCache
 
-if __name__ == "__main__":
+if name == "main":
     print(" Testing LRU Cache")
 
-    cache = LRUCache(2)  # capacity = 2
+    cache = LRUCache(2)
 
     cache.put(1, 10)
     cache.put(2, 20)
-    print("Get(1):", cache.get(1))  # returns 10
+    print("Get(1):", cache.get(1)) 
 
-    cache.put(3, 30)  # removes key 2
-    print("Get(2):", cache.get(2))  # returns -1 (not found)
+    cache.put(3, 30) 
+    print("Get(2):", cache.get(2))  
 
-    cache.put(4, 40)  # removes key 1
-    print("Get(1):", cache.get(1))  # returns -1 (not found)
-    print("Get(3):", cache.get(3))  # returns 30
-    print("Get(4):", cache.get(4))  # returns 40
+    cache.put(4, 40) 
+    print("Get(1):", cache.get(1))  
+    print("Get(3):", cache.get(3))  
+    print("Get(4):", cache.get(4)) 
